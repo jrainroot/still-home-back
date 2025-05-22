a367473a431634b0d18d940a21a1ef624c468575
장우근 <jrainroot@jang-ugeun-ui-MacBookPro.local>
2025-05-22 14:37:53 +0900

    first commit

---
+++ src/main/java/jrainroot/still_home/StillHomeApplication.java
@@ -0,0 +1,13 @@
+package jrainroot.still_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StillHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StillHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jrainroot/still_home/StillHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package jrainroot.still_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StillHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
