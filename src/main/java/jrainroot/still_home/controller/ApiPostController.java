1a0bfd63e4a52c1731d23cbf92dc43d77e4e32b4
jrainroot <jrainroot@gmail.com>
2025-05-30 18:42:57 +0900

    add Post

---
+++ src/main/java/jrainroot/still_home/controller/ApiPostController.java
@@ -0,0 +1,33 @@
+package jrainroot.still_home.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jrainroot.still_home.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
+public class ApiPostController {
+    @GetMapping("")
+    public List<Post> getPosts() {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post((1L)));
+        posts.add(new Post((2L)));
+        posts.add(new Post((3L)));
+
+        return posts;
+    }
+
+    @GetMapping("/{id}")
+    public Post getPost(@PathVariable("id") Long id) {
+        Post post = new Post((id));
+        return post;
+    }
+}
---
+++ src/main/java/jrainroot/still_home/entity/Post.java
@@ -0,0 +1,10 @@
+package jrainroot.still_home.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Post {
+    private Long id;
+}
