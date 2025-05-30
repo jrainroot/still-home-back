24b673e5b0b443c5b2a02bfc27d244595a4705cf
jrainroot <jrainroot@gmail.com>
2025-05-30 21:55:56 +0900

    add @EnableJpaAuditing

--- src/main/java/jrainroot/still_home/StillHomeApplication.java
+++ src/main/java/jrainroot/still_home/StillHomeApplication.java
@@ -2,8 +2,11 @@ package jrainroot.still_home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+// 해당 어노테이션 추가해야 BaseEntity의 생성일 수정일 자동기록 기능 활성화
+@EnableJpaAuditing
 public class StillHomeApplication {
 
     public static void main(String[] args) {
