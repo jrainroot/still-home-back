35022fc0a307f9b599c14ab149cfb53224c86197
jrainroot <jrainroot@gmail.com>
2025-06-02 11:51:48 +0900

    add delete

--- src/main/java/jrainroot/still_home/controller/ApiPostController.java
+++ src/main/java/jrainroot/still_home/controller/ApiPostController.java
@@ -1,19 +1,17 @@
 package jrainroot.still_home.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jrainroot.still_home.entity.Post;
 import jrainroot.still_home.global.ResultData.ResultData;
 import jrainroot.still_home.service.PostService;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,13 +32,11 @@ public class ApiPostController {
         return ResultData.of("S-1", "success", new PostsResponse(posts));
     }
 
-
     // DTO
     @AllArgsConstructor
     public static class PostResponse {
         private final Post post;
     }
-
 
     @GetMapping("/{id}")
     public ResultData<PostResponse> getPost(@PathVariable("id") Long id) {
@@ -53,5 +49,90 @@ public class ApiPostController {
                 "%d 번 게시물은 존재하지 않습니다.".formatted(id),
                 null
         ));
+    }
+
+    @Data
+    public static class WriteRequest {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String content;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteResponse {
+        private final Post post;
+    }
+    @PostMapping("")
+    public ResultData<WriteResponse> write(@Valid @RequestBody WriteRequest writeRequest) {
+//        System.out.println(writeRequest.getSubject());
+//        System.out.println(writeRequest.getContent());
+        ResultData<Post> writeResult = this.postService.create(writeRequest.getTitle(), writeRequest.getContent());
+        if (writeResult.isFail()) return (ResultData) writeResult;
+
+        return ResultData.of(
+                writeResult.getResultCode(),
+                writeResult.getMsg(),
+                new WriteResponse(writeResult.getData())
+        );
+    }
+
+    @Data
+    public static class ModifyRequest {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String content;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ModifyResponse {
+        private final Post post;
+    }
+
+    @PatchMapping("/{id}")
+    public ResultData modify(@Valid @RequestBody ModifyRequest modifyRequest, @PathVariable("id") Long id) {
+        Optional<Post> optionalPost = this.postService.findById(id);
+
+        if(optionalPost.isEmpty()) return ResultData.of(
+                "F-1",
+                "%d번 게시물은 존재하지 않습니다.".formatted(id),
+                null
+        );
+
+        // 회원 권한 체크 canModify();
+
+        ResultData<Post> modifyResult = this.postService.modify(optionalPost.get(), modifyRequest.getTitle(), modifyRequest.getContent());
+
+        return ResultData.of(
+                modifyResult.getResultCode(),
+                modifyResult.getMsg(),
+                new ModifyResponse(modifyResult.getData())
+        );
+    }
+
+
+    @AllArgsConstructor
+    @Getter
+    public static class DeleteResponse {
+        private final Post post;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResultData<DeleteResponse> delete(@PathVariable("id") Long id) {
+        Optional<Post> optionalPost = this.postService.findById(id);
+        if(optionalPost.isEmpty()) return ResultData.of(
+                "F-1",
+                "%d번 게시물은 존재하지 않습니다.".formatted(id),
+                null
+        );
+        ResultData<Post> deleteResult = postService.deleteById(id);
+        return ResultData.of(
+                deleteResult.getResultCode(),
+                deleteResult.getMsg(),
+                new DeleteResponse(optionalPost.get())
+        );
     }
 }
--- src/main/java/jrainroot/still_home/service/PostService.java
+++ src/main/java/jrainroot/still_home/service/PostService.java
@@ -1,6 +1,8 @@
 package jrainroot.still_home.service;
 
+import jakarta.transaction.Transactional;
 import jrainroot.still_home.entity.Post;
+import jrainroot.still_home.global.ResultData.ResultData;
 import jrainroot.still_home.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,11 +23,38 @@ public class PostService {
        return this.postRepository.findById(id);
     }
 
-    public void create(String subject, String content) {
+    @Transactional
+    public ResultData<Post> create(String title, String content) {
         Post post = Post.builder()
-                .title(subject)
+                .title(title)
                 .content(content)
                 .build();
         this.postRepository.save(post);
+        return ResultData.of(
+                "S-3",
+                "게시물이 생성되었습니다.",
+                post
+        );
+    }
+
+    public ResultData<Post> modify(Post post, String title, String content) {
+        post.setTitle(title);
+        post.setContent(content);
+        this.postRepository.save(post);
+
+        return ResultData.of(
+                "S-4",
+                "%d번 게시물이 수정되었습니다.".formatted(post.getId()),
+                post
+        );
+    }
+
+    public ResultData<Post> deleteById(Long id) {
+        this.postRepository.deleteById(id);
+        return ResultData.of(
+                "S-4",
+                "%d번 게시물이 삭제되었습니다.".formatted(id),
+                null
+        );
     }
 }
