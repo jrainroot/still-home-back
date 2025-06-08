c5cc8572ae3402e5c0537936a4c3d3317c54f027
jrainroot <jrainroot@gmail.com>
2025-06-08 14:59:35 +0900

    add accessToken Cookie 등록

--- src/main/java/jrainroot/still_home/controller/ApiMemberController.java
+++ src/main/java/jrainroot/still_home/controller/ApiMemberController.java
@@ -7,12 +7,15 @@ import jrainroot.still_home.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
@@ -37,9 +40,24 @@ public class ApiMemberController {
     }
 
     @PostMapping("/login")
-    public ResultData<LoginResponseBody> login(@Valid @RequestBody LoginRequestBody loginRequestBody) {
+    public ResultData<LoginResponseBody> login(@Valid @RequestBody LoginRequestBody loginRequestBody, HttpServletResponse resp) {
         // username, password => accessToken
         ResultData<MemberService.AuthAndMakeTokenResponseBody> authAndMakeTokenResultData = memberService.authAndMakeTokens(loginRequestBody.getName(), loginRequestBody.getPassword());
+        
+        
+
+        ResponseCookie cookie =  ResponseCookie.from("accessToken", authAndMakeTokenResultData.getData().getAccessToken())
+            .path("/")
+            .sameSite("None")
+            .secure(true)
+            .httpOnly(true) // 프론트에서 직접적인 접근이 안되어 보안상 좋음 
+            .build();
+        resp.addHeader("Set-Cookie", cookie.toString());    
+        
+        // header에 token 추가
+        // resp.addHeader("Set-Cookie", authAndMakeTokenResultData.getData().getAccessToken());
+        
+        
         return ResultData.of(
             authAndMakeTokenResultData.getResultCode(),
             authAndMakeTokenResultData.getMsg(),
