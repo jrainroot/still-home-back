3985bc943cf3c6367183f7bac809caf56ba25a81
jrainroot <jrainroot@gmail.com>
2025-06-09 10:31:42 +0900

    JwtAuthorizationFilter - 토큰을 가지고 접근했을 때 인가해주기

---
+++ src/main/java/jrainroot/still_home/global/security/SecurityUser.java
@@ -0,0 +1,31 @@
+package jrainroot.still_home.global.security;
+
+import java.util.Collection;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class SecurityUser extends User{
+    private long id;
+
+    public SecurityUser(long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.id = id;
+    }
+
+    public SecurityUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.id = id;
+    }
+
+    public Authentication genAuthentication() {
+      Authentication auth = new UsernamePasswordAuthenticationToken(
+                                                                      this, 
+                                                                      this.getPassword(), 
+                                                                      this.getAuthorities());
+      return auth;
+    }
+
+}
